95f4609c2c38107efd7228d5d1e356b8594c5594
Maximalxx <maksimalniy05@gmail.com>
2021-01-19 22:25:08 +0200

    first dop task

---
+++ Module 1/JS/task-1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = 'true';
+console.log(typeof str, typeof num, typeof flag, typeof txt);
---
+++ Module 1/JS/task-2.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let name1 = 'hello';
+let name2 = 1234;
+let name3 = true;
+let name4 = 'true';
+let name5 = 4321;
+const name6 = 'hello';
+const name7 = 1234;
+const name8 = true;
+const name9 = 'true';
+const name0 = 4321;
+
+console.log(name1, name2, name3, name4, name5, name6, name7, name8, name9, name0)
