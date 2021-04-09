f9b8635d6f62e5cb43a34a60efa5ebf15e88f6cf
Maximalxx <maksimalniy05@gmail.com>
2021-04-09 20:49:06 +0300

    hmw 4 added

---
+++ Module 4/Homework4/js/task2.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const inventory = {
+    items: ['Knife', 'Gas mask'],
+    add(itemName) {
+        console.log(`Adding ${itemName} to inventory`);
+
+        inventory.items.push(itemName);
+    },
+    remove(itemName) {
+        console.log(`Removing ${itemName} from inventory`);
+
+        inventory.items = inventory.items.filter(item => item !== itemName);
+    },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+    console.log(`Invoking action on ${itemName}`);
+    action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
