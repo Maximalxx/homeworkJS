949c08f3d70c74cf40b6c9ee7d0fca48d6e7dbaf
Maximalxx <maksimalniy05@gmail.com>
2021-04-15 20:50:31 +0300

    debag

--- Module 2/Homework2/js/task5.js
+++ Module 2/Homework2/js/task5.js
@@ -1,16 +1,12 @@
 'use strict';
 
 const checkForSpam = function (message) {
-    // if (message.includes('spam'.toLocaleLowerCase || 'sale'.toLocaleLowerCase)) {
-    //     return true
-    // } else {
-    //     return false
-    // }
+    
     message = message.toLowerCase();
     const spamWord1 = 'spam';
     const spamWord2 = 'sale';
 
-    if (message.includes(spamWord1 || spamWord2)) {
+    if (message.includes(spamWord1) || message.includes(spamWord2)) {
         return true
     } else {
         return false
